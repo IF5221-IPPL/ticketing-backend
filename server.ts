4eff67a3db63d3ba09748fcd060036cd7f2a56e4
Muhammad Helmi Hibatullah <helmihibatullah52@gmail.com>
2024-03-10 19:02:37 +0700

    initial commit

---
+++ server.ts
@@ -0,0 +1,25 @@
+const express = require("express");
+const swaggerJsdoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+
+// Create a new Express.js app
+const app = express();
+// Configure the app to use Swagger
+const swaggerOptions = {
+  swaggerDefinition: {
+    openapi: "3.0.0",
+    info: {
+      title: "My Express.js API",
+      version: "1.0.0",
+      description: "A sample Express.js API built with TypeScript and Swagger",
+    },
+  },
+  apis: ["./src/routes/*.ts"],
+};
+const swaggerDocs = swaggerJsdoc(swaggerOptions);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+// Define your routes and controllers here
+// Start the Express.js app
+app.listen(3000, () => {
+  console.log("Server started on http://localhost:3000");
+});
