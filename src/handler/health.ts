8998c3e47870fcf8c55a9b920e440ed4ba69ecbe
M. Daffa Argakoesoemah <argakoesoemahmdaffa@gmail.com>
2024-03-19 21:14:11 +0700

    refactor: delete unused code

--- src/handler/health.ts
+++ src/handler/health.ts
@@ -2,18 +2,12 @@ import { IHealthCheck } from "entity/health/";
 import { StatusCodes } from "http-status-codes";
 
 import { sendResponse } from "pkg/http/";
-import { Logger }  from "pkg/logger/";
 
 export const healthCheck = (req: Request, res: Response) => {
 	try {
 		const response: IHealthCheck = {
 			backend: true,
 		};
-
-		// Logger.info({
-  		// 	message: 'User login attempt',
-		// })
-
 
 		sendResponse(res, StatusCodes.OK, "Server is up and running", response);
 	} catch (err) {
