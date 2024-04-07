68bc951757d249853c4d4949ae51edc9f55f79d8
M. Daffa Argakoesoemah <argakoesoemahmdaffa@gmail.com>
2024-04-07 15:26:33 +0700

    feat: add validate login request middleware

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -2,12 +2,13 @@ import { Router } from "express";
 import * as registerHandler from "handler/register";
 import * as loginHandler from "handler/login";
 import { validateRegisterRequest } from "middleware/register/";
+import { validateLoginRequest } from "middleware/login/";
 
 export default () => {
 	const router = Router();
 
 	router.post("/register", validateRegisterRequest, registerHandler.register);
-	router.post("/login", loginHandler.login);
+	router.post("/login", validateLoginRequest, loginHandler.login);
 
 	return router;
 };
