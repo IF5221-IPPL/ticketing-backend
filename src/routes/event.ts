886985297e61f7e206e95440a1937c6457bdd5ea
I Putu Pawesi Siantika, S.T. <pawesisiantika98@gmail.com>
2024-04-19 00:14:04 +0800

    [FEAT] add auth for events API. Only EO role can utilize it

--- src/routes/event.ts
+++ src/routes/event.ts
@@ -1,15 +1,17 @@
-// In routes/event.ts
-
 import express from 'express';
 import { createEvent, readEvents, findEventById, updateEventById, deleteEventById } from '../handler/event';
 import { validateEventData, validateEventUpdateData } from '../middleware/event';
+import { auth } from '../middleware/auth';
+import { checkRole } from 'middleware/check_role/';
 
 const router = express.Router();
 
-router.post('/event', validateEventData, createEvent);
-router.get('/events', readEvents);
-router.get('/event/:eventId', findEventById);
-router.put('/event/:eventId', validateEventUpdateData,updateEventById); 
-router.delete('/event/:eventId', deleteEventById); 
+const EO_ROLE = "EO";
+
+router.post('/event',  auth, checkRole(EO_ROLE), validateEventData, createEvent);
+router.get('/events',  auth, checkRole(EO_ROLE),  readEvents);
+router.get('/event/:eventId', auth, checkRole(EO_ROLE), findEventById);
+router.put('/event/:eventId',  auth, checkRole(EO_ROLE), validateEventUpdateData,updateEventById); 
+router.delete('/event/:eventId',  auth, checkRole(EO_ROLE), deleteEventById); 
 
 export default router;
