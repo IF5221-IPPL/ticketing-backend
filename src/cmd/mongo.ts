03ca134a8b90670a49ce04a86397866505638b7f
michaelpege <mpg066@gmail.com>
2024-05-08 18:59:04 +0700

    [FIX] Config for production db conn

--- src/cmd/mongo.ts
+++ src/cmd/mongo.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 require("dotenv").config();
 
 const db_port = process.env.DB_PORT;
-const db_host = "localhost";
+const db_host = process.env.DF_HOST ?? "localhost";
 const db_user = process.env.DB_USERNAME;
 const db_password = process.env.DB_PASSWORD;
 const db_name = process.env.DB_NAME;
