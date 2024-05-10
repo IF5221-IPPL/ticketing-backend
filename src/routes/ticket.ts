bd283febcc996cc28db559a2c408a3312cc869fd
M. Daffa Argakoesoemah <argakoesoemahmdaffa@gmail.com>
2024-05-10 21:48:09 +0700

    feat: add get ticket detail api

--- src/routes/ticket.ts
+++ src/routes/ticket.ts
@@ -1,7 +1,8 @@
 import CONSTANT from "entity/const/";
 import express from "express";
 import { buyTicket } from "handler/buy_ticket/";
-import { getTickets } from "handler/get_ticket/";
+import { getTickets } from "handler/get_tickets/";
+import { getTicketDetail } from "handler/get_ticket_detail/";
 import { auth } from "middleware/auth/";
 import { checkRole } from "middleware/check_role/";
 
@@ -19,6 +20,13 @@ router.get(
     auth,
     checkRole(CONSTANT.ROLE.CUSTOMER),
     getTickets
+)
+
+router.get(
+    "/tickets/:ticketId",
+    auth,
+    checkRole(CONSTANT.ROLE.CUSTOMER),
+    getTicketDetail
 )
 
 export default router;
