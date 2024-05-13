c5336696270c5a276b4f2798c041a41afbb06e80
I Putu Pawesi Siantika, S.T. <pawesisiantika98@gmail.com>
2024-05-13 17:13:48 +0700

    view accounts with pagination

--- src/handler/account_management.ts
+++ src/handler/account_management.ts
@@ -9,28 +9,69 @@ import User from "model/user/";
 
 
 export const viewAccounts = async (req: Request, res: Response) => {
-     
-    try {
-      const customers = await User.find().lean();
-      const eventOrganizer = await EventOrganizer.find().lean();
-      
-      const accounts = (customers as any[]).concat(eventOrganizer as any[]);
+    const { page = 1, limit = 10 } = req.query;
 
-      sendResponse(
-        res,
-        StatusCodes.OK,
-        "",
-        {
-            accounts:accounts
-        }
-      );
+    try {
+        const totalUsers = await User.countDocuments();
+        const totalEventOrganizers = await EventOrganizer.countDocuments();
+        const totalAccounts = totalUsers + totalEventOrganizers;
+
+        const totalPages = Math.ceil(totalAccounts / limit);
+        const skip = (page - 1) * limit;
+
+        const customers = await User.find().skip(skip).limit(limit).lean();
+        const remainingLimit = Math.max(limit - customers.length, 0);
+        const eventOrganizers = await EventOrganizer.find().skip(skip - totalUsers > 0 ? skip - totalUsers : 0).limit(remainingLimit).lean();
+
+        const accounts = [...customers, ...eventOrganizers];
+
+        sendResponse(
+            res,
+            StatusCodes.OK,
+            "Accounts retrieved successfully",
+            {
+                accounts: accounts,
+                totalAccounts: totalAccounts,
+                currentPage: parseInt(page, 10),
+                totalPages: totalPages
+            }
+        );
     } catch (error) {
-    //   logError(req, res, "Error fetching events", error);
-      sendResponse(
-        res,
-        StatusCodes.INTERNAL_SERVER_ERROR,
-        "Internal Server Error",
-        null
-      );
+        logError(req, res, "Error fetching accounts", error);
+        sendResponse(
+            res,
+            StatusCodes.INTERNAL_SERVER_ERROR,
+            "Internal Server Error",
+            null
+        );
     }
-  };
+};
+
+
+  
+// Helper function for logging error
+function logError(
+    req: Request,
+    res: Response,
+    errorMessage: string,
+    errorObject: any
+  ) {
+    Logger.error({
+      message: errorMessage,
+      request: {
+        url: req.url,
+        method: req.method,
+        headers: req.headers,
+        body: req.body,
+      },
+      response: {
+        statusCode: res.statusCode,
+        headers: res.getHeaders(),
+      },
+      error: {
+        message: errorObject.message,
+        stack: errorObject.stack,
+      },
+    });
+  }
+  
